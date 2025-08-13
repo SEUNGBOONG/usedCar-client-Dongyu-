f4dec554a886a7f332770ca4febcf398219d0d52
승엽 <tmdduqflfl@naver.com>
2025-08-13 19:33:55 +0900

    Feat:

---
+++ dongyuCar/src/main/java/com/example/dongyucar/DongyuCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.dongyucar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongyuCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongyuCarApplication.class, args);
+    }
+
+}
---
+++ dongyuCar/src/test/java/com/example/dongyucar/DongyuCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dongyucar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongyuCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
