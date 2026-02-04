99aa8ab8e837a6caaaa871bceecf2228ebc40a2e
승엽 <tmdduqflfl@naver.com>
2026-02-04 17:36:50 +0900

    Refactoring

--- dongyuCar/src/main/java/com/example/dongyucar/config/WebMvcConfig.java
+++ dongyuCar/src/main/java/com/example/dongyucar/config/WebMvcConfig.java
@@ -25,8 +25,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:3000"); // 개발용
         config.addAllowedOrigin("https://localhost:3000"); // 개발용
-        config.addAllowedOrigin("https://ridethiscar.com"); // 배포용
-        config.addAllowedOrigin("https://ldg.ridethiscar.com"); // 배포용
+        config.addAllowedOrigin("https://승계랜드.com"); // 배포용
+        config.addAllowedOrigin("https://승계랜드.com"); // 배포용
         config.setAllowCredentials(true); // 세션(Cookie) 공유 허용
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
