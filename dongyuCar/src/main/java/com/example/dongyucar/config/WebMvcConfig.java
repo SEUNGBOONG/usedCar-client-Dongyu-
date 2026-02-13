17a5b177740656446b94661ec1487ac23c288c32
승엽 <tmdduqflfl@naver.com>
2026-02-13 18:52:44 +0900

    Refactoring

--- dongyuCar/src/main/java/com/example/dongyucar/config/WebMvcConfig.java
+++ dongyuCar/src/main/java/com/example/dongyucar/config/WebMvcConfig.java
@@ -27,6 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         config.addAllowedOrigin("https://localhost:3000"); // 개발용
         config.addAllowedOrigin("https://승계랜드.com"); // 배포용
         config.addAllowedOrigin("https://승계랜드.com"); // 배포용
+        config.addAllowedOrigin("https://ldg.승계랜드.com"); // 배포용
         config.setAllowCredentials(true); // 세션(Cookie) 공유 허용
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
