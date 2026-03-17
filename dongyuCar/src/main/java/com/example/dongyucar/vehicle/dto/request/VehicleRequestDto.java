e5be5658e51a5bc6047ad004919f084f4aaca76b
승엽 <tmdduqflfl@naver.com>
2026-03-17 18:49:36 +0900

    Refactoring

--- dongyuCar/src/main/java/com/example/dongyucar/vehicle/dto/request/VehicleRequestDto.java
+++ dongyuCar/src/main/java/com/example/dongyucar/vehicle/dto/request/VehicleRequestDto.java
@@ -25,4 +25,43 @@ public class VehicleRequestDto {
 
     private List<MultipartFile> images;
     private List<String> options;
+
+    // ---- Compatibility fields (form-data / legacy clients) ----
+    // Some clients send snake_case or different names; bind and normalize here.
+    private String content;          // = description
+    private String fuel_type;        // = fuelType
+    private String gear_type;        // = gearType
+    private Integer accident_history; // = accidentHistory
+    private Integer month_fee;       // = monthFee
+    private Integer support_fee;     // = supportFee
+
+    public void setContent(String content) {
+        this.content = content;
+        if (this.description == null) this.description = content;
+    }
+
+    public void setFuel_type(String fuelType) {
+        this.fuel_type = fuelType;
+        if (this.fuelType == null) this.fuelType = fuelType;
+    }
+
+    public void setGear_type(String gearType) {
+        this.gear_type = gearType;
+        if (this.gearType == null) this.gearType = gearType;
+    }
+
+    public void setAccident_history(Integer accidentHistory) {
+        this.accident_history = accidentHistory;
+        if (this.accidentHistory == null) this.accidentHistory = accidentHistory;
+    }
+
+    public void setMonth_fee(Integer monthFee) {
+        this.month_fee = monthFee;
+        if (this.monthFee == null) this.monthFee = monthFee;
+    }
+
+    public void setSupport_fee(Integer supportFee) {
+        this.support_fee = supportFee;
+        if (this.supportFee == null) this.supportFee = supportFee;
+    }
 }
