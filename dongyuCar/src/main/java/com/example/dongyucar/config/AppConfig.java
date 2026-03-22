2fbde1dee34b26b1cfff7e6a52fb1fec23f414cd
승엽 <tmdduqflfl@naver.com>
2026-03-22 23:44:03 +0900

    Refactoring

--- dongyuCar/src/main/java/com/example/dongyucar/config/AppConfig.java
+++ dongyuCar/src/main/java/com/example/dongyucar/config/AppConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class AppConfig {
@@ -15,7 +16,8 @@ public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        // Use Spring's Jackson builder so JavaTimeModule and defaults are applied.
+        return Jackson2ObjectMapperBuilder.json().build();
     }
 
 }
