191d8b9940db178dd890f66e405f60bf15356e24
승엽 <tmdduqflfl@naver.com>
2026-04-14 18:23:32 +0900

    Refactoring

--- dongyuCar/src/main/java/com/example/dongyucar/config/SwaggerConfig.java
+++ dongyuCar/src/main/java/com/example/dongyucar/config/SwaggerConfig.java
@@ -28,8 +28,7 @@ public class SwaggerConfig {
                                 )
                 )
                 .servers(List.of(
-                        new Server().url("http://localhost:8080"),
-                        new Server().url("https://api.승계랜드.com")
+                        new Server().url("/")
                 ))
                 .info(new Info()
                         .title("Demo API 명세서")
